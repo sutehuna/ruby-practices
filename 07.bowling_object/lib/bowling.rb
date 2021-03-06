4783d854e2daa9b8e25f6f85c181cb08dbc4196c
sutehuna <sutehuna@gmail.com>
2021-03-06 15:38:24 +0900

    得点計算部分をクラスにしてクラスメソッド化

--- 07.bowling_object/lib/bowling.rb
+++ 07.bowling_object/lib/bowling.rb
@@ -1,44 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative './shot'
-require_relative './frame'
+require_relative './game'
 
-score = ARGV[0]
-shots = score.split(',').map.with_index do |s, i|
-  if s == 'X'
-    Shot.new(i, 10)
-  else
-    Shot.new(i, s.to_i)
-  end
-end
-
-current = 0
-frames = []
-(0..9).each do |i|
-  if shots[current].point == 10 # strike
-    frames << if i == 9
-                Frame.new(i, shots[current], shots[current + 1], shots[current + 2])
-              else
-                Frame.new(i, shots[current])
-              end
-    current += 1
-  else
-    frames << Frame.new(i, shots[current], shots[current + 1])
-    current += 2
-  end
-end
-
-frames.each.with_index do |f, i|
-  current_time = f.shot1.time
-  f.score = if f.shot1.point == 10 || (f.shot1.point + f.shot2&.point || 0) == 10 # strike or spare
-              shots[current_time].point +
-                shots[current_time + 1].point +
-                shots[current_time + 2].point +
-                frames[i - 1]&.score
-            else
-              f.shot1.point + f.shot2.point + frames[i - 1]&.score
-            end
-end
-
-puts frames[9].score
+puts Game.result_for(ARGV[0])
