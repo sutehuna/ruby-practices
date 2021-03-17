133940a1881e951c7b3a279866660843a3f9559e
sutehuna <sutehuna@gmail.com>
2021-03-17 20:13:56 +0900

    不必要なinclude、変数の除去。整形部分をダックタイピングで呼び出すように変更

--- 08.ls_object/lib/text_generator.rb
+++ 08.ls_object/lib/text_generator.rb
@@ -6,13 +6,6 @@ require_relative './long_formatter'
 
 # class for generating output text
 class TextGenerator
-  include ShortFormatter
-  include LongFormatter
-
-  @options = []
-  @current_dir = ''
-  attr_accessor :options
-
   def initialize(options, current_dir)
     @options = options
     @current_dir = current_dir
@@ -21,14 +14,14 @@ class TextGenerator
   def generate_text
     # Create an array of the FileStat class
     files = make_names.map { |f| FileStat.new(f, create_file_stat(f)) }.sort_by(&:name)
-    files = files.reverse if options.include?(:r)
-    options.include?(:l) ? LongFormatter.transform(files) : ShortFormatter.transform(files)
+    files = files.reverse if @options.include?(:r)
+    formatter.transform(files)
   end
 
   private
 
   def make_names
-    if options.include?(:a)
+    if @options.include?(:a)
       Dir.children(@current_dir) + ['.', '..']
     else
       Dir.children(@current_dir).reject { |f| f[0] == '.' }
@@ -37,5 +30,9 @@ class TextGenerator
 
   def create_file_stat(filename)
     File.lstat((@current_dir / filename).to_path)
+  end
+
+  def formatter
+    @options.include?(:l) ? LongFormatter : ShortFormatter
   end
 end
